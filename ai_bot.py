04a7cada74c21fdfebb4330fcf80e0291df0c15b
Josh Chacksfield <joshchacksfield@pvh.com>
2020-02-23 14:43:47 +0100

    Moved noise to utils.py

--- ai_bot.py
+++ ai_bot.py
@@ -11,30 +11,8 @@ from robots.robot.utils import Move, Turn
 
 os.chdir('../robocode_robot')
 
-from model import Model
+from a3c_model import Model
 
-
-
-class OrnsteinUhlenbeckActionNoise:
-    def __init__(self, mu, sigma=0.3, theta=.15, dt=1e-2, x0=None):
-        self.theta = theta
-        self.mu = mu
-        self.sigma = sigma
-        self.dt = dt
-        self.x0 = x0
-        self.reset()
-
-    def __call__(self):
-        x = self.x_prev + self.theta * (self.mu - self.x_prev) * self.dt + \
-            self.sigma * np.sqrt(self.dt) * np.random.normal(size=self.mu.shape)
-        self.x_prev = x
-        return x
-
-    def reset(self):
-        self.x_prev = self.x0 if self.x0 is not None else np.zeros_like(self.mu)
-
-    def __repr__(self):
-        return 'OrnsteinUhlenbeckActionNoise(mu={}, sigma={})'.format(self.mu, self.sigma)
 
 
 class AiRobot(SignalRobot):

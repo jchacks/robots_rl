2835562d81584d2ffa50f2a22be0bda4289c1a77
Josh Chacksfield <joshchacksfield@pvh.com>
2020-02-23 12:33:57 +0100

    Updated battle instantiation to handle new api from robots

--- collect_data.py
+++ collect_data.py
@@ -2,5 +2,5 @@ from robots.app import App, Battle
 from ai_bot import AiRobot
 
 app = App((1280, 720))
-app.battle = Battle(app, (1280, 720), [AiRobot, AiRobot])
+app.battle = Battle(app, (1280, 720), [AiRobot(name='0'), AiRobot(name='1')])
 app.on_execute()

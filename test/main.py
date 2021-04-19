10e991f74748c736c2c6b2cca03de793aeb3d493
Joshua Chacksfield <chacksfieldj@gmail.com>
2021-04-19 17:06:24 +0200

    Added logging

--- test/main.py
+++ test/main.py
@@ -27,7 +27,7 @@ else:
 
 # Simplify battles
 eng.bullet_collisions_enabled = False
-eng.gun_heat_enabled = False
+eng.gun_heat_enabled = True
 
 def get_obs(r):
     direction = np.sin(r.bearing * np.pi / 180), np.cos(r.bearing * np.pi / 180)
@@ -46,10 +46,10 @@ def discounted(rewards, dones, last_value, gamma=0.99):
 
 
 max_steps = 200
-for i in range(10000):
+for episode in range(10000):
     # Create a memory per player
     eng.init()
-    memory = {r: Memory('rewards,a_turning,a_shoot,values,obs,dones') for r in robots}
+    memory = {r: Memory('rewards,action,values,obs,dones') for r in robots}
     steps = 0
     total_reward = {r:0 for r in robots}
     while not eng.is_finished():
@@ -59,21 +59,23 @@ for i in range(10000):
         obs = [get_obs(r) for r in robots]
         obs = [tf.concat([obs[0], obs[1]], axis=0), tf.concat([obs[1], obs[0]], axis=0)]
         obs_batch = tf.stack(obs)
-        moving, turning, shoot = model.actor.sample(obs_batch)
-        shoot = shoot.numpy()
+        action = model.sample(obs_batch)
         value = critic(obs_batch).numpy()
 
         for i, robot in enumerate(robots):
             # Apply actions
+            # Dimension Size: Shoot = 2, Turning = 3
+            shoot = action[i] % 2
+            turning = action[i] // 2
             if robot.turret_heat > 0:
-                shoot[i] = -1
+                shoot = 0
             try:
                 # robot.moving = moving_opts[moving[i]]
-                robot.base_turning = turning_opts[turning[i]]
-                robot.should_fire = shoot[i] > 0
+                robot.base_turning = turning_opts[turning]
+                robot.should_fire = shoot > 0
                 robot.previous_energy = robot.energy
             except Exception:
-                print("Failed assigning actions", i, turning[i], shoot[i])
+                print("Failed assigning actions", i, turning, shoot)
                 raise
 
         eng.step()
@@ -85,15 +87,14 @@ for i in range(10000):
             total_reward[robot] += reward
             memory[robot].append(
                 rewards=reward,
-                # a_moving=moving[i],
-                a_turning=turning[i],
-                a_shoot=shoot[i],
+                action=action[i],
                 values=value[i],
                 obs=obs[i],
                 dones=eng.is_finished()
             )
 
         if eng.is_finished() or (steps % max_steps == 0):
+            #! TRAIN
             steps = 0
             obs = [get_obs(r) for r in robots]
             obs = [tf.concat([obs[0], obs[1]], axis=0), tf.concat([obs[1], obs[0]], axis=0)]
@@ -101,31 +102,57 @@ for i in range(10000):
             last_values = model.critic(obs_batch).numpy()
 
             b_rewards = []
-            b_moving = None
-            b_turning = []
-            b_shoot = []
+            b_action = []
             b_values = []
             b_obs = []
 
             for robot, last_value in zip(robots, last_values):
                 mem = memory[robot]
                 b_rewards.append(discounted(np.array(mem['rewards']), np.array(mem['dones']),+ last_value, 0.99))
-                # b_moving.append(mem['a_moving'])
-                b_turning.append(mem['a_turning'])
-                b_shoot.append(mem['a_shoot'])
+                b_action.append(mem['action'])
                 b_values.append(mem['values'])
                 b_obs.append(mem['obs'])
 
             b_rewards = tf.concat(b_rewards, axis=0)[:,tf.newaxis]
-
-            # b_moving = tf.concat(b_moving, axis=0)
-            b_turning = tf.concat(b_turning, axis=0)
-            b_shoot = tf.concat(b_shoot, axis=0)
-
+            b_action = tf.concat(b_action, axis=0)
             b_values = tf.concat(b_values, axis=0)
             b_obs = tf.concat(b_obs, axis=0)
 
-            losses = model.train(b_obs, b_rewards, (b_moving, b_turning, b_shoot), b_values)
+            losses = model.train(b_obs, b_rewards, b_action, b_values)
             if np.isnan(losses[0].numpy()):
                 raise RuntimeError
             print(f"Total: {losses[0]}, Actor: {losses[1]}, Critic: {losses[2]},  Entropy: {losses[3]}, Reward: {total_reward.values()}")
+    
+    #! Play 1 round without Train and Sampling kill if reaches rount 400
+    if episode % 10 ==0:
+        eng.init()
+        step = 0
+        print("Running test")
+        while not eng.is_finished() and step < 400:
+            if render:
+                app.step()
+
+            obs = [get_obs(r) for r in robots]
+            obs = [tf.concat([obs[0], obs[1]], axis=0), tf.concat([obs[1], obs[0]], axis=0)]
+            obs_batch = tf.stack(obs)
+            action = model.run(obs_batch)
+            value = critic(obs_batch).numpy()
+
+            for i, robot in enumerate(robots):
+                # Apply actions
+                # Dimension Size: Shoot = 2, Turning = 3
+                shoot = action[i] % 2
+                turning = action[i] // 2
+                if robot.turret_heat > 0:
+                    shoot = 0
+                try:
+                    # robot.moving = moving_opts[moving[i]]
+                    robot.base_turning = turning_opts[turning]
+                    robot.should_fire = shoot > 0
+                    robot.previous_energy = robot.energy
+                except Exception:
+                    print("Failed assigning actions", i, turning, shoot)
+                    raise
+
+            eng.step()
+            step += 1
--- test/model.py
+++ test/model.py
@@ -34,9 +34,7 @@ class Actor(tf.Module):
         self.d2 = layers.Dense(256, activation='relu')
         self.d3 = layers.Dense(256, activation='relu')
         self.d4 = layers.Dense(256, activation='relu')
-        # self.moving = layers.Dense(3, activation='softmax')
-        self.turning = layers.Dense(3, activation='softmax')
-        self.shoot = layers.Dense(2, activation='softmax')
+        self.o = layers.Dense(3*2, activation='softmax')
 
     @tf.Module.with_name_scope
     def __call__(self, obs):
@@ -44,63 +42,29 @@ class Actor(tf.Module):
         latent = self.d2(latent)
         latent = self.d3(latent)
         latent = self.d4(latent)
+        return self.o(latent)
 
-        moving = None  # self.moving(latent)
-        turning = self.turning(latent)
-        shoot = self.shoot(latent)
-        return moving, turning, shoot
-
-    def sample(self, obs):
-        moving, turning, shoot = self(obs)
-        # pd_moving = distributions.Categorical(probs=moving)
-        pd_turning = distributions.Categorical(probs=turning)
-        pd_shoot = distributions.Categorical(probs=shoot)
-        return None, pd_turning.sample(), pd_shoot.sample()
-
-    def log_prob(self, obs, actions):
-        amoving, aturning, ashoot = actions
-        moving, turning, shoot = self(obs)
-        # pd_moving = distributions.Categorical(probs=moving)
-        pd_turning = distributions.Categorical(probs=turning)
-        pd_shoot = distributions.Categorical(probs=shoot)
-        return (  # pd_moving.log_prob(amoving),
-            pd_turning.log_prob(aturning),
-            pd_shoot.log_prob(ashoot))
-
-    def entropy(self, obs):
-        moving, turning, shoot = self(obs)
-        # pd_moving = distributions.Categorical(probs=moving)
-        pd_turning = distributions.Categorical(probs=turning)
-        pd_shoot = distributions.Categorical(probs=shoot)
-        return tf.reduce_mean(pd_turning.entropy()) + tf.reduce_mean(pd_shoot.entropy())
-
-
+class Model(tf.Module):
+    def __init__(self, name='model'):
+        super().__init__(name=name)
+        self.actor = Actor()
+        self.critic = Critic()   
+    
 actor = Actor()
 critic = Critic()
 
 
-def get_distributions(observations):
-    moving, turning, shoot = actor(observations)
-    pd_turning = distributions.Categorical(probs=turning)
-    pd_shoot = distributions.Categorical(probs=shoot)
-    return pd_turning, pd_shoot
+def sample(observations):
+    probs = actor(observations)
+    pd_action = distributions.Categorical(probs=probs)
+    return pd_action.sample()
+
+def run(observations):
+    probs = actor(observations)
+    return tf.argmax(probs, axis=1)
 
 
-def actor_loss(observations, actions, advantage):
-    a_move, a_turn, a_shoot = actions
-    move, turn, shoot = actor(observations)
-    pd_turn = distributions.Categorical(probs=turn)
-    pd_shoot = distributions.Categorical(probs=shoot)
-    
-    log_prob_turn = pd_turn.log_prob(a_turn)
-    log_prob_shoot = pd_shoot.log_prob(a_shoot)
-    shoot_loss = (-advantage * log_prob_shoot)[a_shoot == -1]
-
-    # I am not sure what the correct combination is mb adding them together?
-    return -advantage * tf.reduce_sum(log_probs, axis=0)[:, tf.newaxis]
-
-
-def train(observations, rewards, actions, values, norm_advs=False):
+def train(observations, rewards, actions, values, norm_advs=False, print_grads=False):
     """[summary]
 
     Args:
@@ -111,31 +75,33 @@ def train(observations, rewards, actions, values, norm_advs=False):
     """
     observations = tf.cast(observations, tf.float32)
     rewards = tf.cast(rewards, tf.float32)
+    
 
-    advs = rewards - values
+    advantage = rewards - values
     if norm_advs:
-        advs = (advs - tf.reduce_mean(advs)) / (tf.math.reduce_std(advs) + 1e-8)
+        advantage = (advantage - tf.reduce_mean(advantage)) / (tf.math.reduce_std(advantage) + 1e-8)
 
     with tf.GradientTape() as tape:
+        probs = actor(observations)
+        pd = distributions.Categorical(probs=probs)
         vpred = critic(observations)
-        
-        a_losses = actor_loss(observations, actions, advs)
+
+        a_losses = -advantage * pd.log_prob(actions)[:,tf.newaxis]
         a_loss = tf.reduce_mean(a_losses)
 
         # Value function loss
         c_losses = (vpred - rewards) ** 2
         c_loss = tf.reduce_mean(c_losses)
 
-        entropy_reg = actor.entropy(observations)
-        loss = a_loss + c_loss - entropy_reg * 0.5
+        entropy_reg = tf.reduce_mean(pd.entropy())
+        loss = a_loss + c_loss - entropy_reg * 0.3
 
     training_variables = tape.watched_variables()
     grads = tape.gradient(loss, training_variables)
-    for g, v in zip(grads, training_variables):
-        max_g = tf.reduce_max(g)
-        tf.print(v.name, max_g)
-        if tf.math.is_nan(max_g):
-            raise Exception
+    if print_grads:
+        for g, v in zip(grads, training_variables):
+            max_g = tf.reduce_max(g)
+            tf.print(v.name, max_g)
     grads_and_vars = zip(grads, training_variables)
     optimiser.apply_gradients(grads_and_vars)
     return loss, a_loss, c_loss, entropy_reg, tf.reduce_mean([tf.reduce_mean(g) for g in grads])

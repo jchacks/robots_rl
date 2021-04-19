3780dc7b88a0cf1fe6a70194f20051828671f36b
Joshua Chacksfield <chacksfieldj@gmail.com>
2021-04-19 21:41:35 +0200

    Restructured training code

--- test/main.py
+++ test/main.py
@@ -7,13 +7,16 @@ import numpy as np
 import tensorflow as tf
 import tqdm
 from wrapper import Dummy, AITrainingBattle
-from utils import Memory
+from utils import Memory, discounted
+import time
 
 turning_opts = [Turn.LEFT, Turn.NONE, Turn.RIGHT]
 moving_opts = [Move.FORWARD, Move.NONE, Move.BACK]
 
+
 robots = [Dummy((255, 0, 0)), Dummy((0, 255, 0))]
 size = (300, 300)
+
 
 render = True
 if render:
@@ -29,30 +32,91 @@ else:
 eng.bullet_collisions_enabled = False
 eng.gun_heat_enabled = True
 
+
 def get_obs(r):
     direction = np.sin(r.bearing * np.pi / 180), np.cos(r.bearing * np.pi / 180)
     return tf.cast(tf.concat([[r.energy/100, r.turret_heat/30], direction, r.position/size], axis=0), tf.float32)
 
+
 def get_position(r):
     return tf.cast(r.position/size, tf.float32)
 
-def discounted(rewards, dones, last_value, gamma=0.99):
-    discounted = []
-    r = last_value
-    for reward, done in zip(rewards[::-1], dones[::-1]):
-        r = reward + gamma * r * (1.0 - done)
-        discounted.append(r)
-    return np.concatenate(discounted[::-1])
 
-
-max_steps = 200
-for episode in range(10000):
-    # Create a memory per player
+def test(max_steps=200):
     eng.init()
+    eng.set_rate(60)
+    step = 0
+    print("Running test")
+    while not eng.is_finished() and step < max_steps:
+        # Calculate time to sleep
+        time.sleep(max(0, eng.next_sim - time.time()))
+        if render:
+            app.step()
+
+        obs = [get_obs(r) for r in robots]
+        obs = [tf.concat([obs[0], obs[1]], axis=0), tf.concat([obs[1], obs[0]], axis=0)]
+        obs_batch = tf.stack(obs)
+        action = model.run(obs_batch)
+        value = critic(obs_batch).numpy()
+
+        for i, robot in enumerate(robots):
+            # Apply actions
+            # Dimension Size: Shoot = 2, Turning = 3
+            shoot, turning = np.unravel_index(action[i], (2, 3))
+            if robot.turret_heat > 0:
+                shoot = 0
+            try:
+                # robot.moving = moving_opts[moving[i]]
+                robot.base_turning = turning_opts[turning]
+                robot.should_fire = shoot > 0
+                robot.previous_energy = robot.energy
+            except Exception:
+                print("Failed assigning actions", i, turning, shoot)
+                raise
+
+        eng.step()
+        step += 1
+    eng.set_rate(-1)
+
+
+def train(memory):
+    # Get obs for the last state
+    obs = [get_obs(r) for r in robots]
+    obs = [tf.concat([obs[0], obs[1]], axis=0), tf.concat([obs[1], obs[0]], axis=0)]
+    obs_batch = tf.stack(obs)
+    last_values = model.critic(obs_batch).numpy()
+
+    b_rewards = []
+    b_action = []
+    b_values = []
+    b_obs = []
+
+    for robot, last_value in zip(robots, last_values):
+        mem = memory[robot]
+        b_rewards.append(discounted(np.array(mem['rewards']), np.array(mem['dones']), + last_value, 0.99))
+        b_action.append(mem['action'])
+        b_values.append(mem['values'])
+        b_obs.append(mem['obs'])
+
+    b_rewards = tf.concat(b_rewards, axis=0)[:, tf.newaxis]
+    b_action = tf.concat(b_action, axis=0)
+    b_values = tf.concat(b_values, axis=0)
+    b_obs = tf.concat(b_obs, axis=0)
+
+    losses = model.train(b_obs, b_rewards, b_action, b_values)
+    if np.isnan(losses[0].numpy()):
+        raise RuntimeError
+    print(f"{losses[0]}, Actor: {losses[1]}, Critic: {losses[2]},  Entropy: {losses[3]}")
+
+
+eng.init()
+total_reward = {r: 0 for r in robots}
+max_steps = 100
+for iteration in range(10000):
+    # Create a memory per player
     memory = {r: Memory('rewards,action,values,obs,dones') for r in robots}
     steps = 0
-    total_reward = {r:0 for r in robots}
-    while not eng.is_finished():
+    while steps <= max_steps:
         if render:
             app.step()
 
@@ -65,8 +129,7 @@ for episode in range(10000):
         for i, robot in enumerate(robots):
             # Apply actions
             # Dimension Size: Shoot = 2, Turning = 3
-            shoot = action[i] % 2
-            turning = action[i] // 2
+            shoot, turning = np.unravel_index(action[i], (2, 3))
             if robot.turret_heat > 0:
                 shoot = 0
             try:
@@ -93,66 +156,12 @@ for episode in range(10000):
                 dones=eng.is_finished()
             )
 
-        if eng.is_finished() or (steps % max_steps == 0):
-            #! TRAIN
-            steps = 0
-            obs = [get_obs(r) for r in robots]
-            obs = [tf.concat([obs[0], obs[1]], axis=0), tf.concat([obs[1], obs[0]], axis=0)]
-            obs_batch = tf.stack(obs)
-            last_values = model.critic(obs_batch).numpy()
+        if eng.is_finished():
+            print(f"Reward: {list(total_reward.values())}")
+            total_reward = {r: 0 for r in robots}
+            # Episode is over so test
+            if iteration % 10 == 0:
+                test()
+            eng.init()
 
-            b_rewards = []
-            b_action = []
-            b_values = []
-            b_obs = []
-
-            for robot, last_value in zip(robots, last_values):
-                mem = memory[robot]
-                b_rewards.append(discounted(np.array(mem['rewards']), np.array(mem['dones']),+ last_value, 0.99))
-                b_action.append(mem['action'])
-                b_values.append(mem['values'])
-                b_obs.append(mem['obs'])
-
-            b_rewards = tf.concat(b_rewards, axis=0)[:,tf.newaxis]
-            b_action = tf.concat(b_action, axis=0)
-            b_values = tf.concat(b_values, axis=0)
-            b_obs = tf.concat(b_obs, axis=0)
-
-            losses = model.train(b_obs, b_rewards, b_action, b_values)
-            if np.isnan(losses[0].numpy()):
-                raise RuntimeError
-            print(f"Total: {losses[0]}, Actor: {losses[1]}, Critic: {losses[2]},  Entropy: {losses[3]}, Reward: {total_reward.values()}")
-    
-    #! Play 1 round without Train and Sampling kill if reaches rount 400
-    if episode % 10 ==0:
-        eng.init()
-        step = 0
-        print("Running test")
-        while not eng.is_finished() and step < 400:
-            if render:
-                app.step()
-
-            obs = [get_obs(r) for r in robots]
-            obs = [tf.concat([obs[0], obs[1]], axis=0), tf.concat([obs[1], obs[0]], axis=0)]
-            obs_batch = tf.stack(obs)
-            action = model.run(obs_batch)
-            value = critic(obs_batch).numpy()
-
-            for i, robot in enumerate(robots):
-                # Apply actions
-                # Dimension Size: Shoot = 2, Turning = 3
-                shoot = action[i] % 2
-                turning = action[i] // 2
-                if robot.turret_heat > 0:
-                    shoot = 0
-                try:
-                    # robot.moving = moving_opts[moving[i]]
-                    robot.base_turning = turning_opts[turning]
-                    robot.should_fire = shoot > 0
-                    robot.previous_energy = robot.energy
-                except Exception:
-                    print("Failed assigning actions", i, turning, shoot)
-                    raise
-
-            eng.step()
-            step += 1
+    train(memory)
--- test/utils.py
+++ test/utils.py
@@ -1,5 +1,8 @@
+from functools import reduce
+import operator
 import logging
 import tqdm
+import numpy as np
 
 class TqdmLoggingHandler(logging.Handler):
     def __init__(self, level=logging.NOTSET):
@@ -13,29 +16,38 @@ class TqdmLoggingHandler(logging.Handler):
         except (KeyboardInterrupt, SystemExit):
             raise
         except:
-            self.handleError(record)  
+            self.handleError(record)
+
 
 def make_logger():
-    logger = logging.getLogger("")
-    
+    logger = logging.getLogger()
+    logger.setLevel("DEBUG")
     logger.addHandler(TqdmLoggingHandler())
-
     return logger
 
 
 class Memory(object):
-    def __init__(self, stores:str) -> None:
+    def __init__(self, stores: str) -> None:
         self.items = stores.split(',')
-        self.data = {k:[] for k in self.items}
+        self.data = {k: [] for k in self.items}
 
     def append(self, **kwargs):
         if set(kwargs.keys()) != set(self.data.keys()):
             raise KeyError("kwargs keys should be the same as data")
-        for k,v in kwargs.items():
+        for k, v in kwargs.items():
             self.data[k].append(v)
-    
+
     def __getitem__(self, item):
         return self.data[item]
 
     def clear(self):
-        self.data = {k:[] for k in self.items}
+        self.data = {k: [] for k in self.items}
+
+
+def discounted(rewards, dones, last_value, gamma=0.99):
+    discounted = []
+    r = last_value
+    for reward, done in zip(rewards[::-1], dones[::-1]):
+        r = reward + gamma * r * (1.0 - done)
+        discounted.append(r)
+    return np.concatenate(discounted[::-1])
